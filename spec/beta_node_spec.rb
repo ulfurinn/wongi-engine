bb79980a6efbd5f2f14cfd2977a841725b5ad4e9
Valeri Sokolov <valeri.sokolov@snowmen.se>
2015-01-26 09:44:29 +0100

    added a spec to illustrate #tokens being enumerable

---
+++ spec/beta_node_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Wongi::Engine::BetaNode do
+
+  let( :engine ) { Wongi::Engine.create }
+
+  describe '#tokens' do
+
+    it 'should be enumerable' do
+
+      production = engine << rule {
+        forall {
+          has :x, :y, :Z
+        }
+      }
+
+      engine << [:x, :y, 1]
+      engine << [:x, :y, 2]
+      engine << [:x, :y, 3]
+      zs = production.tokens.map { |token| token[:Z] }
+      expect( zs ).to be == [1, 2, 3]
+
+    end
+
+  end
+
+end
