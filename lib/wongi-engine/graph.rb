4deb31e5b1485e31cbc45c718278b43afc72d644
Valeri Sokolov <ulfurinn@ulfurinn.net>
2022-10-04 00:21:15 +0200

    pass an argument instead of using an ivar

--- lib/wongi-engine/graph.rb
+++ lib/wongi-engine/graph.rb
@@ -14,16 +14,12 @@ module Wongi::Engine
         return
       end
 
-      @io = io
+      io.puts "digraph {"
 
-      @io.puts "digraph {"
+      dump_alphas(io) unless opts[:alpha] == false
+      dump_betas(io, opts)
 
-      dump_alphas(opts) unless opts[:alpha] == false
-      dump_betas(opts)
-
-      @io.puts "}"
-    ensure
-      @io = nil
+      io.puts "}"
     end
 
     private
@@ -32,34 +28,34 @@ module Wongi::Engine
       h.to_s.gsub(/-/, '_')
     end
 
-    def dump_alphas(_opts)
-      @io.puts "subgraph cluster_alphas {"
+    def dump_alphas(io)
+      io.puts "subgraph cluster_alphas {"
       @rete.alphas.reject { |alpha| alpha.betas.empty? }.each do |alpha|
-        @io.puts "node#{print_hash alpha.object_id} [shape=box label=\"#{alpha.template.to_s.gsub(/"/, '"')}\"];"
+        io.puts "node#{print_hash alpha.object_id} [shape=box label=\"#{alpha.template.to_s.gsub(/"/, '"')}\"];"
       end
-      @io.puts "};"
+      io.puts "};"
     end
 
-    def dump_betas(opts)
-      dump_beta @rete.beta_top, opts
+    def dump_betas(io, opts)
+      dump_beta(io, @rete.beta_top, opts)
     end
 
-    def dump_beta(beta, opts)
+    def dump_beta(io, beta, opts)
       return if @seen_betas.include? beta
 
       @seen_betas << beta
-      @io.puts "node#{print_hash beta.object_id} [label=\"#{beta.class.name.split('::').last}\"];"
+      io.puts "node#{print_hash beta.object_id} [label=\"#{beta.class.name.split('::').last}\"];"
       if beta.is_a? NccNode
-        @io.puts "node#{print_hash beta.partner.object_id} -> node#{print_hash beta.object_id};"
-        @io.puts "{ rank=same; node#{print_hash beta.partner.object_id} node#{print_hash beta.object_id} }"
+        io.puts "node#{print_hash beta.partner.object_id} -> node#{print_hash beta.object_id};"
+        io.puts "{ rank=same; node#{print_hash beta.partner.object_id} node#{print_hash beta.object_id} }"
       end
       if beta.respond_to?(:alpha) && opts[:alpha] != false
         alpha = beta.alpha
-        @io.puts "node#{print_hash alpha.object_id} -> node#{print_hash beta.object_id};" if alpha
+        io.puts "node#{print_hash alpha.object_id} -> node#{print_hash beta.object_id};" if alpha
       end
       beta.children.each do |child|
-        @io.puts "node#{print_hash beta.object_id} -> node#{print_hash child.object_id};"
-        dump_beta child, opts
+        io.puts "node#{print_hash beta.object_id} -> node#{print_hash child.object_id};"
+        dump_beta(io, child, opts)
       end
     end
   end
